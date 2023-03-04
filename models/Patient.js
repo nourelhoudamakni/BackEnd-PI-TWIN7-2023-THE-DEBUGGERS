6fba0986bd8dde55cf88daa02ad386ad3a7db17e
hamzahafaiedh <hamza.hafaiedh@esprit.tn>
2023-03-04 14:21:07 +0100

    user,doctor and patient models

--- models/Patient.js
+++ models/Patient.js
@@ -1,1 +1,10 @@
-console.log("patient");
+const mongoose=require('mongoose');
+const User=require('./User');
+
+
+const PatientSchema = new mongoose.Schema({
+});
+
+const Patient = User.discriminator('Patient', PatientSchema);
+
+  module.exports=Patient;
