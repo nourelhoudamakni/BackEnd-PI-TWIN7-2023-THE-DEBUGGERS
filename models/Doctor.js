d3bb6311085e9a0d5e5ab83100fc1735d64fe6b2
hamzahafaiedh <hamza.hafaiedh@esprit.tn>
2023-03-04 15:17:13 +0100

    relations avec le doctor

--- models/Doctor.js
+++ models/Doctor.js
@@ -1,11 +1,40 @@
 const mongoose=require('mongoose');
 const User=require('./User');
+const Schema=mongoose.Schema;
 
 
 const DoctorSchema = new mongoose.Schema({
     WorkTime:{
     type:[Date]     
-    }
+    },
+    Patients:[{
+      type:Schema.Types.ObjectId,
+      ref:"Patient"
+    }],
+    MedicalRecord:[{
+      type:Schema.Types.ObjectId,
+      ref:"MedicalRecord"
+    }],
+    Prescriptions:[{
+      type:Schema.Types.ObjectId,
+      ref:"Prescription"
+    }],
+    Notifications:[{
+      type:Schema.Types.ObjectId,
+      ref:"Notification"
+    }],
+    Chats:[{
+      type:Schema.Types.ObjectId,
+      ref:"Chat"
+    }],
+    Appointments:[{
+      type:Schema.Types.ObjectId,
+      ref:"Appointment"
+    }],
+    Service:{
+      type:Schema.Types.ObjectId,
+      ref:"HospitalService"
+  }
   });
 
   const Doctor = User.discriminator('Doctor', DoctorSchema);
