6cb9ad046b2a0a4f5f1363ab0846eacc8dedabec
yesserbenouhiba <yasser.benouhiba@esprit.tn>
2023-03-04 14:13:22 +0100

    relation 2

--- models/Chat.js
+++ models/Chat.js
@@ -1,9 +1,12 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
 const ChatSchema=new mongoose.Schema({
     SenderId:String,
     ReciverId:String,
     Content:String,
     Date:Date,
 });
-const Chat=mongoose.model("chat",userSchema);
 
+const Chat = mongoose.model('Chat', ChatSchema);
 module.exports=Chat;
--- models/Complaint.js
+++ models/Complaint.js
@@ -1,4 +1,5 @@
-const { default: mongoose, Schema } = require("mongoose");
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
 
 const ComplaintSchema=new mongoose.Schema({
     DateofComplaint:Date,
