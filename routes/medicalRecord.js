c4ae9472eb57941cccd2471d8ab0c2054f0b5e52
Oussama Gadria <gadria.oussama@esprit.tn>
2023-03-05 20:32:31 +0100

    Update Medical Record

--- routes/medicalRecord.js
+++ routes/medicalRecord.js
@@ -2,4 +2,5 @@ const express=require('express');
 const router=express.Router(); 
 const medicalRecordControlleur =require('../controllers/medicalRecordController'); 
 router.post("/add",medicalRecordControlleur.upload,medicalRecordControlleur.addMedicalRecord);
+router.put("/update/:medicalRecordId",medicalRecordControlleur.upload,medicalRecordControlleur.updateMedicalRecord)
 module.exports = router;
