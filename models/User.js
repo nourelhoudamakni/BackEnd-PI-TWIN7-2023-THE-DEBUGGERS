bdc089c3f5b6b97c8e65d827f397583e3e0aa39c
yesserbenouhiba <yasser.benouhiba@esprit.tn>
2023-03-09 01:26:57 +0100

    Final changes

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose');
+const {isEmail}=require('validator')  //isEmail mawjouda deja f west validator
+const bcrypt=require("bcrypt")
 
 
 
@@ -11,22 +13,50 @@ const userSchema=new mongoose.Schema({
     gender:String,
     address:String,
     phoneNumber:String,
-    email:String,
-    password:String,
+    email:{
+        type:String,
+        required:[true,'Please enter an email'],
+        unique:true,
+        lowercase:true,
+        validate:[isEmail,'Please enter a valid email']
+    },
+    password:{
+        type:String,
+        required:[true,'Please enter an password'],
+        minlength:[6,'Minimum password length is 6 characters']
+    },
     confirmPassword:String,
     code:String,
     phoneNotVerif:String,
     role:{
         type:String,
-        enum:['doctor','patient']
+        requried:true,
+        enum:['Doctor','Patient','Admin'],
+        default:'Patient'
     },
     confirmed:{
         type:Boolean,
         defaultValue:false,
     },
+    token:{
+        type:String,
+        default:''
+    }
 },{
     discriminatorKey: 'userType' // set discriminator key to 'userType'
 })
+
+userSchema.statics.login=async function(email,password){            //compare email and password to login
+    const user=await this.findOne({email})
+    if(user){
+        const auth= await bcrypt.compare(password,user.password)         //to compare user password with the stocked password in the database if true:pass if false:dosent pass
+        if(auth){
+            return user
+        }
+        throw Error('incorrect password')
+    }
+    throw Error('incorrect email')
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports=User;
